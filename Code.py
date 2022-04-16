a299c3deb5e3229b55e62f2ca12532511845dc04
FinKing18 <98707457+FinKing18@users.noreply.github.com>
2022-04-16 18:10:04 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,4 +1,7 @@
- pip install streamlit fbprophet yfinance plotly
+pip install streamlit
+pip install fbprophet
+pip install yfinance
+pip install plotly
 import streamlit as st
 from datetime import date
 import time
@@ -42,3 +45,25 @@ def plot_raw_data():
 	st.plotly_chart(fig)
 	
 plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
